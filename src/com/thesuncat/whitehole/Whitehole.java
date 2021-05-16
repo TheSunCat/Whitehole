706828ecd9a132aa02bea21faa76608b5ef52698
IonicPixels <72478475+IonicPixels@users.noreply.github.com>
2021-05-15 23:28:48 -0400

    Revert "Yes" (#2)

--- src/com/thesuncat/whitehole/Whitehole.java
+++ src/com/thesuncat/whitehole/Whitehole.java
@@ -32,9 +32,9 @@ import java.util.prefs.Preferences;
 import javax.swing.*;
 
 public class Whitehole {
-    public static final String NAME = "Spaghettihole v1.6.1";
+    public static final String NAME = "Whitehole v1.6.1";
     public static final String WEBURL = "https://discord.gg/NTyb4sy";
-    public static final String CRASHURL = "IonicPixels#3139";
+    public static final String CRASHURL = "TheSunCat#1007";
     public static final Image ICON = Toolkit.getDefaultToolkit().createImage(Whitehole.class.getResource("/res/icon.png"));
     
     /**
