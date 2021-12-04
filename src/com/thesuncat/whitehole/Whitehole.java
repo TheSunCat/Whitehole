cf0a149ba00e7083f287a13257b4217cf9afa52c
IonicPixels <72478475+IonicPixels@users.noreply.github.com>
2021-12-03 22:43:46 -0500

    change server url

--- src/com/thesuncat/whitehole/Whitehole.java
+++ src/com/thesuncat/whitehole/Whitehole.java
@@ -33,7 +33,7 @@ import javax.swing.*;
 
 public class Whitehole {
     public static final String NAME = "Whitehole v1.7";
-    public static final String WEBURL = "https://discord.gg/NTyb4sy";
+    public static final String WEBURL = "https://discord.gg/r8h5vAm2JC";
     public static final String CRASHURL = "IonicPixels#3139";
     public static final Image ICON = Toolkit.getDefaultToolkit().createImage(Whitehole.class.getResource("/res/icon.png"));
     
